aaaca9d2b4d7c53d374f46a852596e707feadb8f
sunilpowar <sunilpowarsp8468@gmail.com>
2022-01-23 21:28:19 +0530

    Adding Switch Case

--- EmployeeWageComptProb/EmplWage.cs
+++ EmployeeWageComptProb/EmplWage.cs
@@ -6,31 +6,33 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComptProb
 {
-    public static void EmplWage()
+    internal class EmplWage
     {
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
-        int empHr = 0;
-        int empWage = 0;
-        Random random = new Random();
-        int empCheck = random.Next(0, 3);
-        if (empCheck == IS_PART_TIME)
+        public static void EmpAttendance()
         {
-            empHr = 4;
-        }
-        else if (empCheck == IS_FULL_TIME)
-        {
-            empHr = 8;
-        }
-        else
-        {
-            empHr = 0;
-        }
-        empWage = empHr * EMP_RATE_PER_HOUR;
-        Console.WriteLine("Emp Wage" + empWage);
-        Console.ReadLine();
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHr = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHr = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHr = 8;
+            }
+            else
+            {
+                empHr = 0;
+            }
+            empWage = empHr * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage" + empWage);
+            Console.ReadLine();
 
+        }
     }
 }
-
