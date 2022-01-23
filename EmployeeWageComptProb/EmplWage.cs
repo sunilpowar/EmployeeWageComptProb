5858e6870a32ace1aa225ed25ea19376d6d1c773
sunilpowar <sunilpowarsp8468@gmail.com>
2022-01-23 21:36:41 +0530

    Adding Total Working

--- EmployeeWageComptProb/EmplWage.cs
+++ EmployeeWageComptProb/EmplWage.cs
@@ -12,11 +12,13 @@ namespace EmployeeWageComptProb
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
-        int empHrs = 0, empWage = 0, totalEmpWage;
+        public const int MAX_HRS_IN_MONTH = 100;
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWage;
         public void MonthlyEmpWage()
         {
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -31,10 +33,10 @@ namespace EmployeeWageComptProb
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee Wage : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Employee Hours : " + empHrs);
             }
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Employee Wage : " + totalEmpWage);
         }
     }
