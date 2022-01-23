61bd865eaaf4033b13b8e6d851c3aeb729b97cf1
sunilpowar <sunilpowarsp8468@gmail.com>
2022-01-23 21:16:22 +0530

    Emplyee Attendace

--- EmployeeWageComptProb/EmplWage.cs
+++ EmployeeWageComptProb/EmplWage.cs
@@ -11,17 +11,23 @@ namespace EmployeeWageComptProb
         public static void EmpAttendance()
         {
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHr = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                empHr = 8;
             }
             else
             {
-                Console.WriteLine("Employee is not present");
+                empHr = 0;
             }
+            empWage = empHr * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage" + empWage);
             Console.ReadLine();
+
         }
     }
 }
