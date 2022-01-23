40ed20bf56c3dd365bed9147d5dba3f7c9ffc118
sunilpowar <sunilpowarsp8468@gmail.com>
2022-01-23 21:23:58 +0530

    Adding Parttime

--- EmployeeWageComptProb/EmplWage.cs
+++ EmployeeWageComptProb/EmplWage.cs
@@ -6,28 +6,31 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComptProb
 {
-    internal class EmplWage
+    public static void EmplWage()
     {
-        public static void EmpAttendance()
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHr = 0;
+        int empWage = 0;
+        Random random = new Random();
+        int empCheck = random.Next(0, 3);
+        if (empCheck == IS_PART_TIME)
         {
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            int empHr = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-            {
-                empHr = 8;
-            }
-            else
-            {
-                empHr = 0;
-            }
-            empWage = empHr * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage" + empWage);
-            Console.ReadLine();
-
+            empHr = 4;
         }
+        else if (empCheck == IS_FULL_TIME)
+        {
+            empHr = 8;
+        }
+        else
+        {
+            empHr = 0;
+        }
+        empWage = empHr * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage" + empWage);
+        Console.ReadLine();
+
     }
 }
+
